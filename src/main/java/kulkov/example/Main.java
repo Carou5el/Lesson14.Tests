d38080071df705e2712547980798d1e39af0e013
Garry Deepcool <garrydeepcool@outlook.com>
2021-02-24 10:18:12 +0300

    Task 1 is finished.

--- src/main/java/kulkov/example/Main.java
+++ src/main/java/kulkov/example/Main.java
@@ -1,36 +1,67 @@
 package kulkov.example;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class Main {
 
     // Тестовый массив.
     static int[] testArray = { 23,234, 234,123, 4, 32, 23 };
+    static int[] testArray1 = { 134,14,3,423,423,42,34,235,74,7,5,3,4,324,2342,34,234,234,34, 4 };
+    static int[] testArray2 = { 1,2,6,4,7,6,7,5,6,5,6 };
+    static int[] testArray3 = { 23, 4, 234,123, 32, 23 };
     static int[] resultArray;
+
     public static void main(String[] args) {
 
-        checkArray(testArray);
+//        checkArray(testArray);
+        splitArray(testArray1, resultArray, checkArray(testArray1));
     }
 
-    static void checkArray(int[] arrayToSplit) throws RuntimeException  {
+    @Test
+    public void testCheckArray()    {
+        Assertions.assertEquals(4, checkArray(testArray));
+    }
+    @Test
+    public void testCheckArray1()    {
+        Assertions.assertEquals(1, checkArray(testArray1));
+    }
+
+    @Test
+    public void testCheckArray2()    {
+        Assertions.assertEquals(3, checkArray(testArray2));
+    }
+
+    @Test
+    public void testCheckArray3()    {
+        Assertions.assertEquals(1, checkArray(testArray3));
+    }
+
+    static int checkArray(int[] arrayToSplit) throws RuntimeException  {
+
+        int result = 0;
+
+        if(arrayToSplit[arrayToSplit.length - 1] == 4)  {
+            throw new RuntimeException();
+        }
 
         for(int i = arrayToSplit.length - 1; i >= 0; i--)   {
 
-            // При обнаружении "4" создаём новый массив, необходимого размера.
             if(arrayToSplit[i] == 4)    {
-                resultArray = new int[(arrayToSplit.length - 1 - i)];
-                splitArray(arrayToSplit, resultArray);
-                return;
+
+                return i;
             }
         }
-        throw new RuntimeException();
 
+        throw new RuntimeException();
     }
 
-    static void splitArray(int[] arrayToSplit, int[] resultArray)    {
+    static void splitArray(int[] arrayToSplit, int[] resultArray, int firstElementPosition)    {
 
         int j = 0;
-        int firstElemPos = arrayToSplit.length - resultArray.length;
+        resultArray = new int[arrayToSplit.length - firstElementPosition - 1];
 
-        for(int i = firstElemPos; i < arrayToSplit.length; i++) {
+        for(int i = firstElementPosition + 1; i < arrayToSplit.length; i++) {
 
             resultArray[j] = arrayToSplit[i];
             j++;
